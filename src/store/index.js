28f1dbb463ef4e0c5196a5a1e95e51aff9b98c71
Pranav <pranavmendi@gmail.com>
2021-07-16 06:08:09 +0530

    Recreate the store using reduxjs/toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,62 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
 
-export const ACTIONS = {
-	INC: 'increment',
-	DEC: 'decrement',
-	TOG: 'toggle',
-	INCBA: 'increaseByAmount',
-};
+// export const ACTIONS = {
+// 	INC: 'increment',
+// 	DEC: 'decrement',
+// 	TOG: 'toggle',
+// 	INCBA: 'increaseByAmount',
+// };
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-	if (action.type === ACTIONS.INC) {
-		return {
-			counter: state.counter + 1,
-			showCounter: state.showCounter,
-		};
-	} else if (action.type === ACTIONS.DEC) {
-		return {
-			counter: state.counter - 1,
-			showCounter: state.showCounter,
-		};
-	} else if (action.type === ACTIONS.INCBA) {
-		return {
-			counter: state.counter + 10,
-			showCounter: state.showCounter,
-		};
-	} else if (action.type === ACTIONS.TOG) {
-		return {
-			counter: state.counter,
-			showCounter: !state.showCounter,
-		};
-	}
-	return state;
-};
+const counterSlice = createSlice({
+	name: 'Counter',
+	initialState,
+	reducer: {
+		increment(state) {
+			state.counter++;
+			// we shouldn't update existing state but redux manages this in the background
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increaseByAmount(state, action) {
+			state.counter += action.payload;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
 
-export const store = createStore(counterReducer);
+// const counterReducer = (state = initialState, action) => {
+// 	if (action.type === ACTIONS.INC) {
+// 		return {
+// 			counter: state.counter + 1,
+// 			showCounter: state.showCounter,
+// 		};
+// 	} else if (action.type === ACTIONS.DEC) {
+// 		return {
+// 			counter: state.counter - 1,
+// 			showCounter: state.showCounter,
+// 		};
+// 	} else if (action.type === ACTIONS.INCBA) {
+// 		return {
+// 			counter: state.counter + action.amount,
+// 			showCounter: state.showCounter,
+// 		};
+// 	} else if (action.type === ACTIONS.TOG) {
+// 		return {
+// 			counter: state.counter,
+// 			showCounter: !state.showCounter,
+// 		};
+// 	}
+// 	return state;
+// };
+
+// export const store = createStore(counterReducer);
+
+export const store = configureStore({ reducer: counterSlice.reducer });
+
+export const counterActions = counterSlice.actions;
